1563766faf1e3871ffe003631e250de1420fcf96
Elshirbini <ahmedalshirbini33@gmail.com>
2024-11-25 22:01:11 +0200

    modified:   .DS_Store 	modified:   frontend/.DS_Store 	new file:   frontend/dist/assets/No-data-B3sngSla.png 	new file:   frontend/dist/assets/add-notes-8jjrwYrN.png 	new file:   frontend/dist/assets/index-B_jc_Sr_.css 	new file:   frontend/dist/assets/index-Bdhsnz7C.js 	new file:   frontend/dist/index.html 	new file:   frontend/dist/vite.svg 	modified:   frontend/src/utils/constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,2 +1,2 @@
 // export const BASE_URL = "http://localhost:8000";
-export const BASE_URL = "https://notes-app-api-nine.vercel.app"
+export const BASE_URL = "https://notes-app-api-nine.vercel.app/"
